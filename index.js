9d0614806520d9f6d35890216abec9420f187aa3
suntao <suntao@DESKTOP-P8IU6JF>
2019-11-05 19:19:40 +0800

    browserWindow

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow } = require('electron')
 
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
-let win
+let win , childWin , modelWin
 /**
  * width
  * height
@@ -12,9 +12,23 @@ let win
  * maxWidth:800,
  * x 初始相对于窗口x的位置
  * y 初始相对于窗口y的位置
+ * fullscreen 全屏窗口
+ * setFullScreen 可动态设置窗口全屏
+ * isFullScreen 判断当前窗口是否全屏
+ * kiosk 窗口锁定  （全屏状态下窗口锁定，mac下Command+q退出，windows下菜单退出）
  * 
- * 
+ * 模态窗口是禁用父窗口的子窗口，创建模态窗口必须设置 parent 和 modal 选项,模态窗口显示时无法点击父窗口
  * */ 
+
+/**
+ * 窗口间的数据交互，使用IPC （interProcess Communication，进程间通讯）方式在窗口间传递数据
+ * ipcMain (用于主窗口)和 ipcRenderer（用于其他窗口） 
+ * 主窗口window1 其他窗口：window2
+ * window1 -》 window2
+ * window2会通过ipcRenderer触发一个事件，用于接收window1传递过来的数据，在window2关闭时，会通过ipcRenderer给window1发送消息，window1
+ * 通过ipcMain接受window2传递过来的数据
+ * 
+ * */  
 function createWindow () {
   // 创建浏览器窗口。
   win = new BrowserWindow({
@@ -22,21 +36,50 @@ function createWindow () {
     height: 600,
     minWidth:300,
     minHeight:400,
-    maxHeight:600,
-    maxWidth:800,
+    title:'研学平台',
+    // backgroundColor:'#fb6501', // 窗口背景颜色
+    // maxHeight:600,
+    // maxWidth:800,
+    // fullscreen:true,
+    // kiosk:true,
     // x:20,
     // y:20,
-    webPreferences: {
-      nodeIntegration: true
+    // frame:false, //有无边框 默认为true 有边框
+    // transparent:true, //是否为透明窗口
+    // alpha:"#41e0c8", //transparent为true方可支持alpha属性，格式为#AARRGGBB)。 默认值为 #FFF（白色）
+    icon:'./src/assets/basketball.ico',
+    show:false, // 是否显示窗口
+    webPreferences: { //网页功能的设置
+      nodeIntegration: true, // 是否集成node
     }
   })
 
+  //创建子窗口  (mac下子窗口会随着父窗口移动，windows下则不会)
+  // childWin = new BrowserWindow({
+  //   parent:win,
+  //   width: 400,
+  //   height: 300,
+  // })
+
+  // modelWin = new BrowserWindow({
+  //   parent:win,
+  //   modal: true,
+  //   width: 400,
+  //   height: 300,
+  //   show:false
+  // })
+
+
   // 加载index.html文件
   win.loadFile('index.html')
-
+  // modelWin.loadFile('child.html')  
   // 打开开发者工具
   win.webContents.openDevTools()
 
+  win.on('ready-to-show', ()=>{ // 页面装载完再显示，这样就不会有窗口空白等待时间
+    win.show();
+  })
+  
   // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
     // 取消引用 window 对象，如果你的应用支持多窗口的话，
--- src/js/event.js
+++ src/js/event.js
@@ -1,4 +1,5 @@
 const remote = require('electron').remote
+const BrowserWindow = remote.BrowserWindow
 /**
  * getSize()  // 获取窗口大小
  * setPosition(width,height,flag)  // flag设置为true ，代表一动画效果设置尺寸（仅限制于mac OS）
@@ -16,4 +17,82 @@ function setSizePosition(params) {
     const win = remote.getCurrentWindow();
     win.setSize(300,300,true)
     win.setPosition(10,10)
+}
+
+function kioskWindow(params) {
+    const win = remote.getCurrentWindow();
+    const btn = document.getElementById('button');
+
+    if (win.isKiosk()) {
+        win.setKiosk(false)
+        btn.innerText = '进入锁定模式'
+    }else{
+        win.setKiosk(true)
+        btn.innerText = '退出锁定模式' 
+    }
+}
+
+
+function createMoreWindow(params) {
+    const win = remote.getCurrentWindow();
+    if (global.windows == undefined) {
+        global.windows = [];
+    }
+    const addWin = new BrowserWindow({show:false,y:20,x:20,width:400,height:340});
+    global.windows.push(addWin);
+    addWin.loadFile('child.html');
+    addWin.on('ready-to-show',()=>{
+        addWin.show();
+    })
+}
+
+// 关闭除了主窗口外的所有窗口   global全局变量
+function closeAllChildWindow(params) {
+    if (global.windows != undefined) {
+        for (let i = 0; i < global.windows.length; i++) {
+           
+            global.windows[i].close()
+        }
+        global.windows.length = 0;
+        global.windows = undefined;
+    }
+}
+
+
+// --------------------------------------
+// 获取ipcmain对象
+const ipcMain = remote.ipcMain;
+const {ipcRenderer} = require('electron')
+
+ipcMain.on('childData',(event,data)=>{
+      const label = document.getElementById('childText');
+      label.innerText =  label.innerText + data;
+})
+
+//  传递数据给子窗口
+function sendData(params) {
+    const childWin = new BrowserWindow({show:false,y:20,x:20,width:400,height:400})
+    childWin.loadFile('child.html');
+    childWin.once('ready-to-show',()=>{
+        childWin.show();
+        childWin.webContents.send('data',{name:"sunTao",age:18})
+    })
+}
+
+
+// 接受主窗口传递过来的数据
+function onloadData(params) {
+    ipcRenderer.on('data',(event, arg) => {
+        console.log("childData:"+arg)
+        const name = document.getElementById('label_name')  
+        const age = document.getElementById('label_age')  
+        name.innerText = arg.name
+        age.innerText = arg.age
+    })
+} 
+
+function closeCurWindow(params) {
+    const win = remote.getCurrentWindow()
+    ipcRenderer.send("childData",'窗口已关闭');
+    win.close();
 }
